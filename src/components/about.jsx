24f1bca379f5eb57d1135328af9478ad8f8db9f6
OmPrakash <om@habilesoft.com>
2020-03-28 23:30:25 +0530

    - update code

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -23,7 +23,7 @@ export default class About extends Component {
             </div>
         </div>
         </section>
-        <section className="colorlib-about">
+        <section className="colorlib-about"  data-section="technology">
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
